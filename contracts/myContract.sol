6b336ad7ffad4708b507c1d2ac8e03f60d9c0a61
Nattapon Sukthaworn <nattapon33998@gmail.com>
2023-03-25 01:31:21 +0700

    feat: add event log

--- contracts/myContract.sol
+++ contracts/myContract.sol
@@ -4,12 +4,15 @@ pragma solidity 0.8.19;
 contract MyContract {
     string private name;
 
+    event NameSet(string newName, address indexed editedBy, uint256 timestamp);
+
     constructor(string memory _name) {
         name = _name;
     }
 
     function setName(string memory _name) public {
         name = _name;
+        emit NameSet(_name, msg.sender, block.timestamp);
     }
 
     function getName() public view returns (string memory) {
